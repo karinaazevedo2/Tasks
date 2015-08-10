69f1c105e0a2317910be10322956ca658674a841
karinaazevedo2 <kari.pereira@yahoo.com.br>
2015-08-10 00:20:28 -0300

    make it better

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -24,7 +24,7 @@ environments {
     }
     test {
         dataSource {
-            dbCreate = "create-drop"
+            dbCreate = "update"
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
